af534b9cdd73b63e0587c3b6b3bb01d90a0c8af1
Julio Correa <juliocesarjr@outlook.com>
2023-10-15 18:54:54 -0300

    REST endpoints and handling the API

--- src/main/java/com/jotaerre/domain/model/User.java
+++ src/main/java/com/jotaerre/domain/model/User.java
@@ -32,6 +32,14 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Account getAccount() {
         return account;
     }
